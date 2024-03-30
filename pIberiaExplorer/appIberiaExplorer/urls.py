9cb39d2b7b3652964fe3e6233628ae80e4b7ff02
GabrielRL <grlgh353@gmail.com>
2024-03-30 19:38:43 +0100

    Gabriel: Login

---
+++ pIberiaExplorer/appIberiaExplorer/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import *
+
+app_name = "appIberiaExplorer"
+
+urlpatterns = [
+    path("", index, name="index"),
+    # CRUD usuario
+    path("register/", register, name="register"),
+    path("login/", login, name="login"),
+    path("logout/", logout, name="logout"),
+    # Confirmar correo
+    path("confirm_email/<str:token>/", confirm_email, name="confirm_email"),
+    path("email_confirmed/", email_confirmed, name="email_confirmed"),
+]
